692c92ccf9cb87bbae16a706233db8230404c410
hanyjay <biolahaneef@gmail.com>
2025-10-21 23:52:38 +0100

    database update

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
+import os
 
-DATABASE_URL = f"postgresql+asyncpg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# DATABASE_URL = f"postgresql+asyncpg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set in environment")
