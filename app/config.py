6abf792b603a21850af8a1a1ed1d8d65d7f6339c
hanyjay <biolahaneef@gmail.com>
2025-10-22 01:12:08 +0100

    update config

--- app/config.py
+++ app/config.py
@@ -1,14 +1,16 @@
 from pydantic_settings import BaseSettings
+from typing import Optional
 
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
-    
+    database_hostname: Optional[str] = None
+    database_port: Optional[str] = None
+    database_password: Optional[str] = None
+    database_name: Optional[str] = None
+    database_username: Optional[str] = None
+
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
